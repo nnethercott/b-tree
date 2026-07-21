10ddb6717d9928bf1b471767b1b8fcfa1ec82eaf
nate <natenethercott@gmail.com>
2026-07-21 07:53:12 +0200

    move Traversal struct inside SlottedPage

--- main.zig
+++ main.zig
@@ -58,79 +58,126 @@ const Offset = struct {
     }
 };
 
-const SlottedPage = struct {
-    header: Header,
-    offsets: std.ArrayList(Offset) = .empty,
-    cells: std.ArrayList(cell(i32, i32)) = .empty,
-    offset: usize = 0,
+fn slotted_page(comptime fanout: usize, comptime k: type, comptime v: type) type {
+    // NOTE: in a v1 we'll use the `fanout` as the ground truth for page size rather than the
+    // "fit everythin into a page" approach
 
-    // fanout: usize
+    return struct {
+        header: Header,
+        offsets: [fanout]cell(k, v) = .{},
+        cells: [fanout]cell(k, v) = .{},
+        offset: usize = 0,
 
-    const Self = @This();
-    const empty: Self = .{ .header = .{} };
+        const Self = @This();
+        const empty: Self = .{ .header = .{} };
 
-    fn insert(self: *Self, gpa: std.mem.Allocator, k: i32, v: i32) !void {
-        // TODO: check if we're at capacity and SPLIT !
-        try self.offsets.append(gpa, .{ .offset = self.offset });
-        try self.cells.insert(gpa, 0, .{ .key = k, .value = v });
+        /// Traversal helper for binary search
+        const Mode = enum {
+            Insert,
+            Search,
+        };
 
-        // FIXME: what if we always allocated here ? std.heap.FixedBufferAllocator.init(buffer: []u8)
-        // and then kept this allocator local to the slotted page ?
-        // still doesn't guarantee we'll get "append from right" behaviour
-        // BUT: could use a fba on the corresponding slice to alloc...
+        const Traversal = struct {
+            /// indicates whether we need crumbs or not
+            mode: Mode,
 
-        // now reorder the self.offsets
-        std.sort.heap(
-            Offset,
-            self.offsets.items[0..],
-            .{ k, self.cells.items[0..] },
-            Offset.lessThanFn,
-        );
-        self.offset += 1;
-    }
+            gpa: ?std.mem.Allocator = null,
 
-    fn get(self: *const Self, key: i32) ?i32 {
-        const kind = self.header.kind;
+            /// stores {ptr, split_idx} as a stack
+            breadcrumbs: std.ArrayList(struct { *Self, usize }) = .empty,
 
-        switch (kind) {
-            .Leaf => return binary_search_value(self, key),
-            .Internal => {
-                const next = binary_search_page(self, key);
-                return next.get(key);
-            },
+            fn assert_mode_ok(self: Traversal) !void {
+                if (self.mode == .Insert and self.gpa == null) {
+                    return error.MissingAllocator;
+                }
+            }
+
+            fn binary_search_value(self: *Traversal, page: *const Self, needle: i32) ?i32 {
+                _ = self;
+
+                const idx = std.sort.binarySearch(
+                    Offset,
+                    page.offsets.items[0..],
+                    .{ needle, page.cells.items[0..] },
+                    Offset.cmpKey,
+                ) orelse return null;
+
+                const offset = page.offsets.items[idx];
+                return page.cells.items[offset.offset].value.?;
+            }
+
+            fn binary_search_page(self: *Traversal, page: *const Self, needle: i32) !*Self {
+                try self.assert_mode_ok();
+
+                const offset_idx = std.sort.upperBound(
+                    Offset,
+                    page.offsets.items[0..],
+                    .{ needle, page.cells.items[0..] },
+                    Offset.cmpKey,
+                );
+
+                const found, const idx = blk: {
+                    const cells = page.cells.items.len;
+
+                    if (offset_idx < cells) {
+                        const idx = page.offsets.items[offset_idx].offset;
+                        break :blk .{ page.cells.items[idx].next_page.?, idx };
+                    }
+                    // otherwise item is on the right
+                    break :blk .{ page.header.right_ptr.?, cells };
+                };
+
+                switch (self.mode) {
+                    .Insert => self.breadcrumbs.append(self.gpa.?, .{ found, idx }) catch return error.FailedToAllocate,
+                    .Search => {},
+                }
+
+                return found;
+            }
+        };
+
+        fn insert(self: *Self, gpa: std.mem.Allocator, key: k, value: v) !void {
+            const t: Traversal = .{ .mode = .Insert, .gpa = gpa };
+            _ = t;
+
+            // t.binary_search_page(self, k)
+
+            if (self.offset == fanout) {
+                return error.OutOfBounds;
+            }
+
+            // TODO: check if we're at capacity and SPLIT !
+            try self.offsets.append(gpa, .{ .offset = self.offset });
+            try self.cells.insert(gpa, 0, .{ .key = key, .value = value });
+
+            // FIXME: what if we always allocated here ? std.heap.FixedBufferAllocator.init(buffer: []u8)
+            // and then kept this allocator local to the slotted page ?
+            // still doesn't guarantee we'll get "append from right" behaviour
+            // BUT: could use a fba on the corresponding slice to alloc...
+
+            // now reorder the self.offsets
+            std.sort.heap(
+                Offset,
+                self.offsets.items[0..],
+                .{ k, self.cells.items[0..] },
+                Offset.lessThanFn,
+            );
+
+            self.offset += 1;
         }
-    }
-};
 
-fn binary_search_value(page: *const SlottedPage, needle: i32) ?i32 {
-    expect(page.header.kind == .Leaf) catch unreachable;
+        fn get(self: *const Self, key: i32) ?i32 {
+            const kind = self.header.kind;
+            var t: Traversal = .{ .mode = .Search };
 
-    const idx = std.sort.binarySearch(
-        Offset,
-        page.offsets.items[0..],
-        .{ needle, page.cells.items[0..] },
-        Offset.cmpKey,
-    ) orelse return null;
-
-    const offset = page.offsets.items[idx];
-    return page.cells.items[offset.offset].value.?;
-}
-
-fn binary_search_page(page: *const SlottedPage, needle: i32) *SlottedPage {
-    expect(page.header.kind == .Internal) catch unreachable;
-
-    const offset_idx = std.sort.upperBound(
-        Offset,
-        page.offsets.items[0..],
-        .{ needle, page.cells.items[0..] },
-        Offset.cmpKey,
-    );
-
-    if (offset_idx < page.cells.items.len) {
-        const idx = page.offsets.items[offset_idx].offset;
-        return page.cells.items[idx].next_page.?;
-    }
-
-    // otherwise item is on the right
-    return page.header.right_ptr.?;
+            switch (kind) {
+                .Leaf => return t.binary_search_value(self, key),
+                .Internal => {
+                    // we never allocate so this cannot fail
+                    const next = t.binary_search_page(self, key) catch unreachable;
+                    return next.get(key);
+                },
+            }
+        }
+    };
 }
